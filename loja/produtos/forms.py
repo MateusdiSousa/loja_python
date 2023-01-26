a3f19bffd9d4c0db5e274c6b30eb1c4a5f9c8242
Mateus de Sousa <mateus69107@gmail.com>
2023-01-26 00:11:54 -0300

    feat: Cadastro de Produtos

---
+++ loja/produtos/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf.file import FileAllowed, FileField,FileRequired
+from wtforms import Form, IntegerField, StringField, BooleanField, TextAreaField, validators
+
+class Addprodutos(Form):
+    name = StringField('Nome: ',[validators.DataRequired()])
+    color = TextAreaField('Cor: ',[validators.DataRequired()])
+    discount = IntegerField('Desconto: ',[validators.DataRequired()])
+    discription = TextAreaField('Descrição: ',[validators.DataRequired()])
+    price = IntegerField('Preço: ',[validators.DataRequired()])
+    stock = IntegerField('Estoque: ',[validators.DataRequired()])
+
+    image_1 = FileField('Imagem 1: ',validators=[FileRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+    image_2 = FileField('Imagem 2: ',validators=[FileRequired(),FileAllowed(['jpg', 'jpeg', 'gif'])])
+    image_3 = FileField('Imagem 3: ',validators=[FileRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+
+    
+    
+    
--- loja/produtos/rotas.py
+++ loja/produtos/rotas.py
@@ -1,5 +1,5 @@
 from flask import request, redirect, render_template, url_for, flash
-
+from .forms import Addprodutos
 from loja import app, db
 from .models import Marca, Categoria
 
@@ -14,3 +14,21 @@ def add_marca():
         flash(f'A marca {getmarca} foi adicionada com sucesso','success')
         return redirect(url_for('add_marca'))
     return render_template("produtos/add_marca.html", title='Cadastrar Marca', marcas='marcas')
+
+@app.route("/add_categoria", methods = ['GET', 'POST'])
+def add_cat():
+
+    if request.method == 'POST':
+        getcategoria = request.form.get('categoria')
+        categoria = Categoria(name=getcategoria)
+        db.session.add(categoria)
+        db.session.commit()
+        flash(f'A categoria {getcategoria} foi adicionada com sucesso','success')
+        return redirect(url_for('add_marca'))
+    return render_template("produtos/add_marca.html", title='Cadastrar Categoria')
+
+@app.route("/add_produto", methods = ['GET', 'POST'])
+def add_produto():
+    form = Addprodutos(request.form)
+    
+    return render_template("produtos/add_produto.html", title='Cadastrar Produto', form = form)

f5a8755c001bd5e233c69c06f6fde6fdbcfb065d
Mateus de Sousa <mateus69107@gmail.com>
2023-01-27 18:36:25 -0300

    feat: cadastro de produto

--- loja/produtos/rotas.py
+++ loja/produtos/rotas.py
@@ -1,8 +1,8 @@
 from flask import request, redirect, render_template, url_for, flash
 from .forms import Addprodutos
 from loja import app, db, photos
-from .models import Marca, Categoria
-import os
+from .models import Marca, Categoria, Ad_produtos
+import os, secrets
 
 @app.route("/add_marca", methods = ['GET', 'POST'])
 def add_marca():
@@ -39,11 +39,31 @@ def add_produto():
     img_3= request.files.get('image_3')
 
     if request.method == 'POST':
+        name = form.name.data
+        color = form.color.data
+        discount = form.discount.data
+        discription = form.discription.data
+        price = form.price.data
+        stock = form.stock.data
+        marca = request.form.get('marca')
+        categoria = request.form.get('categoria')
+
+
         if img_1:
-            photos.save(request.files.get('image_1'))
+           image_1 = photos.save(img_1, name= secrets.token_hex(10)+'.')
+        else:
+            image_1 = ''
         if img_2:
-            photos.save(request.files.get('image_2'))
+           image_2 = photos.save(img_2, name= secrets.token_hex(10)+'.')
+        else:
+            image_2 = ''
         if img_3:    
-            photos.save(request.files.get('image_3'))
+           image_3 =photos.save(img_3, name= secrets.token_hex(10)+'.')
+        else:
+            image_3 = ''
+
+        produto = Ad_produtos(name=name, color=color, discount=discount, discription=discription, price= price, stock=stock, marca_id = marca, categoria_id = categoria, img_1 = image_1, img_2 = image_2, img_3 = image_3 )
+        db.session.add(produto)
         db.session.commit()
+        flash('Produto {} foi adicionada com sucesso!'.format(name),'success')
     return render_template("produtos/add_produto.html", title='Cadastrar Produto', form = form, marcas = marcas, categorias = categorias)
